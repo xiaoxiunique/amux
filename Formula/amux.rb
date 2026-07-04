72b66cdc847663013bba4d0d571d7a5666ee0da1
Atom <929746602@qq.com>
2026-07-04 12:59:00 +0800

    chore: release v0.3.0 (save/restore, auto-save, default resume, fuzzy goto)

--- Formula/amux.rb
+++ Formula/amux.rb
@@ -1,7 +1,7 @@
 class Amux < Formula
   desc "Run AI coding agents in per-directory persistent tmux sessions"
   homepage "https://github.com/xiaoxiunique/amux"
-  url "https://github.com/xiaoxiunique/amux/archive/refs/tags/v0.2.0.tar.gz"
+  url "https://github.com/xiaoxiunique/amux/archive/refs/tags/v0.3.0.tar.gz"
   sha256 "6d3c113328583c9e825453e04462a3118237dae6ee64cb5d11098fc27d6243ea"
   license "MIT"
   depends_on "rust" => :build
