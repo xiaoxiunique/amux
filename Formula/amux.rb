f6961c2b1dd6adfbd36de7989b6973cd7bb60010
Atom <929746602@qq.com>
2026-07-14 10:33:39 +0800

    chore: release v0.5.0 (rmux multiplexer backend)

--- Formula/amux.rb
+++ Formula/amux.rb
@@ -1,7 +1,7 @@
 class Amux < Formula
   desc "Run AI coding agents in per-directory persistent tmux sessions"
   homepage "https://github.com/xiaoxiunique/amux"
-  url "https://github.com/xiaoxiunique/amux/archive/refs/tags/v0.4.3.tar.gz"
+  url "https://github.com/xiaoxiunique/amux/archive/refs/tags/v0.5.0.tar.gz"
   sha256 "8eda204c6b232d4434fc5cdfda15ad2e83af5b3ac60b4a9a5a95db5dbf92f787"
   license "MIT"
   depends_on "rust" => :build
